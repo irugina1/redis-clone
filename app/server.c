ec0c7c5a6585662bd5a22f11007dae18a20a9f0f
Ileana Rugina <ileana.rugina.2@gmail.com>
2023-09-29 23:37:06 -0700

    follow resp

--- app/server.c
+++ app/server.c
@@ -53,11 +53,11 @@ void *handle_client(void *args){
 		}
 		if (cmd->type == CMD_SET) {
 			insert(ht, cmd->args[1], atoi(cmd->args[2]));
-			reply = "OK";
+			reply = "+OK";
 		}
 		if (cmd->type == CMD_GET) {
 			int val = get(ht, cmd->args[1]);
-			sprintf(reply, "%d", val);
+			sprintf(reply, ":%d", val);
 		}
 		else{
 		}
